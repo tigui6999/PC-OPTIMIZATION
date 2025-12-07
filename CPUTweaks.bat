8e33f0bb87a6b0707d3fa20180faae239bac8582
L O G A N <154441675+NEKR1D@users.noreply.github.com>
2025-12-07 04:21:05 -0800

    Update CPUTweaks.bat

--- CPUTweaks.bat
+++ CPUTweaks.bat
@@ -120,93 +120,36 @@ reg add "HKLM\SYSTEM\ControlSet001\Control\Processor" /v "CpuIdleScrubValueDefau
 
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WcmSvc\GroupPolicy" /v "fDisablePowerManagement" /t REG_DWORD /d "1" /f
 
-rem ::: Windows Thread Priorities
+rem ::: Windows CPU Thread Priorities
 rem ::: Threads are scheduled for execution based on their priority, ranging from 0 (lowest) to 31 (highest)
-rem ::: https://learn.microsoft.com/en-us/windows/win32/procthread/scheduling-priorities
 rem ::: 15 = Above Normal - Time Critical / High - Time Critical
 
-rem ::: alufena = 0x0000000f (15)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DXGKrnl\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000000f (15)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\nvlddmkm\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000001f (31)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\mouclass\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\kbdclass\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: NVMe Driver
-rem ::: alufena = 0x00000000
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\stornvme\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x00000000" /f
-
-rem ::: Ancillary Function Driver (AFD)
-rem ::: alufena = 0x00000000
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x00000000" /f
-
-rem ::: alufena = 0x0000000f (15)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000000f (15)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\mouhid\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000001f (31)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\NDIS\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000000f (15)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\kbdhid\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000000f (15)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\HidUsb\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000001f (31)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\USBHUB3\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: SATA Driver / AHCI Driver
-rem ::: Used for SSD and spinny drives
-rem ::: alufena = 0x00000000
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\storahci\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x00000000" /f
-
-rem ::: alufena = 0x00000000
 reg add "HKLM\System\CurrentControlSet\Services\Tcpip6\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x00000000" /f
-
-rem ::: alufena = 0x0000001f (31)
 reg add "HKLM\System\CurrentControlSet\Services\USBXHCI\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000000f
 reg add "HKLM\System\CurrentControlSet\Services\usbhub\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x00000000
 reg add "HKLM\System\CurrentControlSet\Services\Ntfs\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x00000000" /f
-
-rem ::: Intel Rapid Storage Technology Driver (SATA)
-rem ::: alufena = 0x00000000
 reg add "HKLM\System\CurrentControlSet\Services\iaStorAVC\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x00000000" /f
-rem ::: alufena = 0x00000000
 reg add "HKLM\System\CurrentControlSet\Services\iaStorAC\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x00000000" /f
-
-rem ::: SATA Driver
-rem ::: alufena = 0x00000000
 reg add "HKLM\System\CurrentControlSet\Services\disk\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x00000000" /f
-
-rem ::: alufena = 0x00000000
 reg add "HKLM\System\CurrentControlSet\Services\Audiosrv\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x00000000" /f
-
-rem ::: alufena = 0x0000000f
 reg add "HKLM\System\CurrentControlSet\Services\usbuhci\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000000f
 reg add "HKLM\System\CurrentControlSet\Services\usbohci\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000000f
 reg add "HKLM\System\CurrentControlSet\Services\usbehci\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000000f
 reg add "HKLM\System\CurrentControlSet\Services\usbccgp\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000000f
 reg add "HKLM\System\CurrentControlSet\Services\monitor\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
-
-rem ::: alufena = 0x0000000f
 reg add "HKLM\System\CurrentControlSet\Services\HDAudBus\Parameters" /v "ThreadPriority" /t REG_DWORD /d "0x0000000f" /f
 
 PAUSE
